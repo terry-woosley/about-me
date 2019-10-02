bbc896ed874ec3b76e3204a262936435c40a4396
Terry Woosley <s526788@nwmissouri.edu>
2019-10-02 17:30:27 -0500

    redo the pwa again v3

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
